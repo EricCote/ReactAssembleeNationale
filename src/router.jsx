43de9d3428fbaa4848e1f62699f4f77c5143fcaa
Eric Cote <eric@coteexpert.com>
2024-02-07 12:00:36 -0500

    Lazy loading de la page Contact

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './home/Home';
 import News from './news/News';
-import Contact from './contact/Contact';
 import Layout from './Layout';
 import Compteur from './compteur/Page';
 import ErreurNavigation from './ErreurNavigation';
+import { Suspense, lazy } from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Contact = lazy(() => import('./contact/Contact'));
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     errorElement: <ErreurNavigation />,
+
     children: [
       {
         index: true,
@@ -22,7 +26,11 @@ const router = createBrowserRouter([
       },
       {
         path: 'contact',
-        element: <Contact />,
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Contact />
+          </Suspense>
+        ),
       },
       {
         path: 'compteur',
